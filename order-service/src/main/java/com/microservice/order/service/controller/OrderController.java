8e55afd643f98d838f3c9e5070b1d998e10e46fc
kirankumarhm <kirankumarhm2004@gmail.com>
2023-09-09 21:21:35 +0530

    Added Circute Braker Pattern

--- order-service/src/main/java/com/microservice/order/service/controller/OrderController.java
+++ order-service/src/main/java/com/microservice/order/service/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.microservice.order.service.controller;
 
 import com.microservice.order.service.service.OrderService;
 import com.microservice.order.service.dto.OrderRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +16,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
     }
 
-
+    public String placeOrderFallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops!!! Something went wrong, please try later";
+    }
 }
